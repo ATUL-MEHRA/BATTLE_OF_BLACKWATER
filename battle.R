4608c511b5e039ea74d206b308e88c4d8d79e9f5
ATUL MEHRA <39877272+ATUL-MEHRA@users.noreply.github.com>
2020-04-03 20:29:40 +0530

    Update battle.R

--- battle.R
+++ battle.R
@@ -1,4 +1,4 @@
-# The Analysis About the selection of the soldiers that have performed outstanding in the War. The king wants to award the best 
+# The Analysis about the selection of the soldiers that have performed outstanding in the War. The king wants to award the best 
 # soldiers of its Army. I have created the Logistic regression model to find out the Soldiers deserve to be awarded.
 
 train <- read.csv("train.csv")
